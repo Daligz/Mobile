8d796f2d33a295e608dc61555980a570fe346ec8
Daligz <oscardali370@gmail.com>
2022-06-27 15:58:48 -0500

    Firebase connected

--- login_firebase/app/src/main/java/me/upp/daligz/app/login_firebase/MainActivity.java
+++ login_firebase/app/src/main/java/me/upp/daligz/app/login_firebase/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.createDatabase();
+    }
+
+    private void createDatabase() {
+        // Write a message to the database
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
